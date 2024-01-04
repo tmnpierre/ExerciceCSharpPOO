1c1037a2b377e73f96cf02421b2937f4ffcde627
tmnpierre <tomyn.pierre@gmail.com>
2024-01-04 14:54:00 +0100

    feat: exercice01

---
+++ Exercice01Chaise/Classe/Chaise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice01Chaise.Classe
+{
+    internal class Chaise
+    {
+        private int _nbDePied;
+        private string _materiaux;
+        private string _couleur;
+        public int NbDePied { get => _nbDePied; set => _nbDePied = value; }
+        public string Materiaux { get => _materiaux; set => _materiaux = value; }
+        public string Couleur { get => _couleur; set => _couleur = value; }
+
+        public void Afficher()
+        {
+            Console.WriteLine($"La chaise à {_nbDePied}, et de couleur {_couleur} en {_materiaux}");
+        }
+        public Chaise(int NbDePied, string Materiaux, string Couleur)
+        {
+            _nbDePied = NbDePied;
+            _materiaux= Materiaux;
+            _couleur = Couleur;
+        }
+    }
+}
---
+++ Exercice01Chaise/Program.cs
@@ -0,0 +1,14 @@
+﻿using Exercice01Chaise.Classe;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Chaise une = new Chaise(4, "bois", "blanche");
+        une.Afficher();
+        Chaise deux = new Chaise(2, "metal", "noire");
+        deux.Afficher();
+        Chaise trois = new Chaise(6, "plastique", "bleue");
+        trois.Afficher();
+    }
+}
