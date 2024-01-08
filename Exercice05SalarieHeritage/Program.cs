8b40f8d78b714e766c248a1c39eeb12cc7e6a12f
tmnpierre <tomyn.pierre@gmail.com>
2024-01-08 16:39:34 +0100

    feat(menu): ameliorations menu utilisateur

--- Exercice05SalarieHeritage/Program.cs
+++ Exercice05SalarieHeritage/Program.cs
@@ -1,13 +1,60 @@
 ﻿using Exercice05SalarieHeritage.Classes;
 
 Console.WriteLine("--- Gestion des employés --- \n");
-Console.WriteLine("1 -- Ajouter un employé");
-Console.WriteLine("2 -- Afficher le salaire des employés");
-Console.WriteLine("3 -- Rechercher un employé");
-Console.WriteLine("0 -- Quitter \n");
-Console.Write("Entrer votre choix : ");
-var choose = Console.ReadLine();
 
+bool continuer = true;
+
+while (continuer)
+{
+    Console.Clear();
+    Console.WriteLine("1 -- Ajouter un employé");
+    Console.WriteLine("2 -- Afficher le salaire des employés");
+    Console.WriteLine("3 -- Rechercher un employé");
+    Console.WriteLine("0 -- Quitter \n");
+    Console.Write("Entrez votre choix : ");
+    var choose = Console.ReadLine();
+
+    switch (choose)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("--- Ajouter un employé --- \n");
+
+            bool continuer2 = true;
+            while (continuer2)
+            {
+                Console.WriteLine("1 -- Salarié");
+                Console.WriteLine("2 -- Commercial");
+                Console.WriteLine("3 -- Retour \n");
+                Console.Write("Entrez votre choix : ");
+                var choose2 = Console.ReadLine();
+
+                switch (choose2)
+                {
+                    case "1":
+                        // Logic for adding a Salarié
+                        break;
+                    case "2":
+                        // Logic for adding a Commercial
+                        break;
+                    case "3":
+                        continuer2 = false;
+                        Console.Clear();
+                        break;
+                }
+            }
+            break;
+        case "2":
+            // Display employee salaries
+            break;
+        case "3":
+            // Search for an employee
+            break;
+        case "0":
+            continuer = false;
+            break;
+    }
+}
 
 Salarie jeanMichel = new Commercial("004", "Jean-Michel", "Commercial", "Cadre", 1300, 13000, 3);
 
