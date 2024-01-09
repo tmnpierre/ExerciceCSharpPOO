53336796d2e4d34316521868eacb5389ce82f286
tmnpierre <tomyn.pierre@gmail.com>
2024-01-09 12:14:13 +0100

    feat(Exercice06): update class CompteEpargne

--- Exercice06CompteBancaire/Classes/CompteEpargne.cs
+++ Exercice06CompteBancaire/Classes/CompteEpargne.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Exercice06CompteBancaire.Classes
 {
-    internal class CompteEpargne
+    internal class CompteEpargne : CompteBancaire
     {
+        public CompteEpargne(string client, decimal soldeInitial) : base(client, soldeInitial)
+        {
+        }
+
+        public override void Depot(decimal montant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Retrait(decimal montant)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
