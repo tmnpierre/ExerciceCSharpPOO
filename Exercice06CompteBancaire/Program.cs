943422758aae4f9749c7ec4819383d7d9c43a260
tmnpierre <tomyn.pierre@gmail.com>
2024-01-09 14:25:38 +0100

    feat(Exercice06): create IHM and start menu

--- Exercice06CompteBancaire/Program.cs
+++ Exercice06CompteBancaire/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var continuer = true;
+
+Console.WriteLine("=== Menu Principal === \n");
+Console.WriteLine("1. Lister les comptes bancaire");
+Console.WriteLine("2. Creer un compte bancaire");
+Console.WriteLine("3. Effectuer un depot");
+Console.WriteLine("4. Effectuer un retrait");
+Console.WriteLine("5. Afficher les opérations et le solde");
+Console.WriteLine("0. Quitter le programme \n");
+Console.Write("Entrez votre choix : ");
+var choose = Console.ReadLine();
+
+while (continuer)
+{
+    switch (choose)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+        case "0":
+            break;
+    }
+}
