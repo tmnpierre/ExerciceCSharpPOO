eea97e3e7678dec84a03dec0b08c4a2c21481572
tmnpierre <tomyn.pierre@gmail.com>
2024-01-09 12:09:48 +0100

    feat(Exercice06): update class ComptePayant

--- Exercice06CompteBancaire/Classes/ComptePayant.cs
+++ Exercice06CompteBancaire/Classes/ComptePayant.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Exercice06CompteBancaire.Classes
 {
-    internal class ComptePayant
+    internal class ComptePayant : CompteBancaire
     {
+        public ComptePayant(string client, decimal soldeInitial) : base(client, soldeInitial)
+        {
+        }
+
+        public override void Depot(decimal montant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Retrait(decimal montant)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
