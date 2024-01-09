2ac000ef61ff4135b3cf7c5bc1160dcf6e5602aa
tmnpierre <tomyn.pierre@gmail.com>
2024-01-09 14:47:28 +0100

    feat(Exercice06): continue menu

--- Exercice06CompteBancaire/Program.cs
+++ Exercice06CompteBancaire/Program.cs
@@ -1,4 +1,6 @@
-﻿var continuer = true;
+﻿using Exercice06CompteBancaire.Classes;
+
+var continuer = true;
 
 Console.WriteLine("=== Menu Principal === \n");
 Console.WriteLine("1. Lister les comptes bancaire");
@@ -23,6 +25,29 @@ while (continuer)
         case "4":
             break;
         case "5":
+            break;
+        case "0":
+            continuer = false;
+            break;
+    }
+}
+
+static void CreerCompte (List<CompteBancaire> ListeCompte)
+{
+    Console.Clear();
+    Console.WriteLine("1. Créer un compte courant");
+    Console.WriteLine("2. Créer un compte épargne");
+    Console.WriteLine("3. Créer un compte payant");
+    Console.WriteLine("0. Retour au menu principal \n");
+    Console.Write("Entrez votre choix : ");
+    var choose = Console.ReadLine();
+    switch (choose)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
             break;
         case "0":
             break;
