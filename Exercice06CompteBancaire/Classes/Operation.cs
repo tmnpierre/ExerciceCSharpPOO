1b3984fa9783422291bf6bec661aacce9dbb7925
tmnpierre <tomyn.pierre@gmail.com>
2024-01-09 13:30:27 +0100

    feat(Exercice06): update class Operation

--- Exercice06CompteBancaire/Classes/Operation.cs
+++ Exercice06CompteBancaire/Classes/Operation.cs
@@ -8,5 +8,16 @@ namespace Exercice06CompteBancaire.Classes
 {
     internal class Operation
     {
+        public int numero {  get; set; }
+        public decimal montant {  get; set; }
+        public enum Statut {retrait, debot};
+        public Statut statut { get; set; }
+        public Operation(int numero, decimal montant, Statut statut)
+        {
+            this.numero = numero;
+            this.montant = montant;
+            this.statut = statut;
+        }
+
     }
 }
