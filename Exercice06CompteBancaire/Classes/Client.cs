515dc46664f78a06588a5c4a4dc61e5038126f1c
tmnpierre <tomyn.pierre@gmail.com>
2024-01-09 12:20:21 +0100

    feat(Exercice06): update class Client

--- Exercice06CompteBancaire/Classes/Client.cs
+++ Exercice06CompteBancaire/Classes/Client.cs
@@ -8,5 +8,9 @@ namespace Exercice06CompteBancaire.Classes
 {
     internal class Client
     {
+        public string Prenom {  get; set; }
+        public string Nom { get; set; }
+        public string Identifiant { get; set; }
+        public List<string> ListeCompte { get; private set; }
     }
 }
