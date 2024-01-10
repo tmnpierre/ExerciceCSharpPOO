d71603d81d0efe749dcc910ad999132d332b9bf2
tmnpierre <tomyn.pierre@gmail.com>
2024-01-10 11:23:13 +0100

    feat(Exercice07Figure): update interface IDeplacement

--- Exercice07Figure/Interfaces/IDeplacement.cs
+++ Exercice07Figure/Interfaces/IDeplacement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercice07Figure.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Exercice07Figure.Interfaces
 {
     internal interface IDeplacement
     {
+        void Deplacement(double dx, double dy);
     }
 }
