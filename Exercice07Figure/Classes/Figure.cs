182f69d8f52dae418c397b342cfd6dc5e96e3a3a
tmnpierre <tomyn.pierre@gmail.com>
2024-01-10 11:26:17 +0100

    feat(Exercice07Figure): update class Figure

--- Exercice07Figure/Classes/Figure.cs
+++ Exercice07Figure/Classes/Figure.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exercice07Figure.Interfaces;
 
 namespace Exercice07Figure.Classes
 {
     internal abstract class Figure
     {
         public Point origine;
+
+        public void Deplacement(double dx, double dy)
+        {
+            origine.PosX += dx;
+            origine.PosY += dy;
+        }
     }
 }
